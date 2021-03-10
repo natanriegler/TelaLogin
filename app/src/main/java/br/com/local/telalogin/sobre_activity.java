1b7438eccbcd409f6a73454b784aa3c20110b1d1
natan <natan.riegler.silva@gmail.com>
2021-03-10 19:39:56 -0300

    New Activitys

---
+++ app/src/main/java/br/com/local/telalogin/sobre_activity.java
@@ -0,0 +1,14 @@
+package br.com.local.telalogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sobre_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_layout);
+    }
+}
