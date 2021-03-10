170166183948f55ad211d03fe944b06f00d0da20
natan <natan.riegler.silva@gmail.com>
2021-03-10 19:14:17 -0300

    New Activity

---
+++ app/src/main/java/br/com/local/telalogin/cadastro_usuario_activity.java
@@ -0,0 +1,14 @@
+package br.com.local.telalogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cadastro_usuario_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_usuario_layout);
+    }
+}
